0d413e8189c07576a23262f77d7d134daae5e998
Sebastien Menozzi <sebastien.menozzi@epita.fr>
2018-12-23 19:29:08 +0100

    Initial Commit

---
+++ Nearby/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nearby
+//
+//  Created by Sebastien Menozzi on 23/12/2018.
+//  Copyright © 2018 Sebastien Menozzi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
